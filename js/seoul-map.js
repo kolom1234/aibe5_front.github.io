b61f0dd6eafa86f3d198cadb750451b4111088cc
kolom1234 <kolom1357@gmail.com>
2026-01-23 02:34:14 +0900

    Fix: Remove GL mode for Chrome compatibility with custom dark theme

--- js/seoul-map.js
+++ js/seoul-map.js
@@ -150,12 +150,10 @@ const SeoulMap = (function () {
         if (!naverMap) {
             console.log("Initializing Naver Map (Using Native Custom Dark Style)");
             naverMap = new naver.maps.Map('naver-map', {
-                gl: true,
                 center: new naver.maps.LatLng(lat, lng),
                 zoom: 15,
                 mapDataControl: false,
                 scaleControl: false,
-                //isNightModeEnabled: true,
                 customStyleId: '4166f2a1-c2fa-4d09-92ae-13802768e969'
             });
         } else {
